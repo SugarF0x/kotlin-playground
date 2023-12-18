0160c8f181f9780f118a93e042b2116c0e0ad848
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-12-18 21:36:54 +0300

    define additional examples

--- src/test/kotlin/katas/ReverseOrRotate.kt
+++ src/test/kotlin/katas/ReverseOrRotate.kt
@@ -6,14 +6,27 @@ import kotlin.test.assertEquals
 // https://www.codewars.com/kata/56b5afb4ed1f6d5fb0000991/train/kotlin
 
 class ReverseOrRotate {
-    private fun revRot(nums: String, sz: Int): String {
+    private fun revRot(value: String, size: Int): String {
         return ""
     }
 
     @Test
     fun test() {
-        assertEquals(revRot("733049910872815764", 5), "330479108928157")
-        assertEquals(revRot("73304991087281576455176044327690580265896", 8), "1994033775182780067155464327690480265895")
-        assertEquals(revRot("73304991087281576455176044327690580265896896028", 8), "1994033775182780067155464327690480265895")
+        val sample = listOf(
+            Triple("733049910872815764", 5, "330479108928157"),
+            Triple("73304991087281576455176044327690580265896", 8, "1994033775182780067155464327690480265895"),
+            Triple("73304991087281576455176044327690580265896896028", 8, "1994033775182780067155464327690480265895"),
+            Triple("123456987654", 6, "234561876549"),
+            Triple("123456987653", 6, "234561356789"),
+            Triple("66443875", 4, "44668753"),
+            Triple("66443875", 8, "64438756"),
+            Triple("664438769", 8, "67834466"),
+            Triple("123456779", 8, "23456771"),
+            Triple("", 8, ""),
+            Triple("123456779", 0, ""),
+            Triple("563000655734469485", 4, "0365065073456944"),
+        )
+
+        for ((value, size, output) in sample) assertEquals(revRot(value, size), output)
     }
 }
